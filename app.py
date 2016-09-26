da767ee5d297104f5568f4c56269b43daabcbbb7
Sebastian Cain <sebastian.cain.stuy@gmail.com>
2016-09-26 07:27:16 -0400

    final

--- app.py
+++ app.py
@@ -1,26 +1,58 @@
 from flask import Flask, render_template
-
-
+import random
 app = Flask(__name__)
 
 @app.route("/")
 
 def route1():
-	return render_template("index.html", title = "This be the first page")
+	return render_template("index.html", title = "Occu-Table Central")
 
-@app.route("/occu")
+@app.route("/table")
 
 def route2():
-	return render_template("occu.html", title = "this is the second route.")
+	return render_template("table.html", title = "this is the table route.", tableData = getTableMatrix())
 
-def getOccuData():
+@app.route("/random")
+
+def route3():
+	return render_template("random.html", title = "this is the random route.", occupation = getRandom())
+
+def getTableMatrix():
     occu = open("occupations.csv", "r")
     streamy = occu.read()
 
+    matrix = [[]]
+    
     data = streamy.strip().split("\n")
 
     for line in range(len(data)):
-        line.strip('"')
+        
+        lineData = data[line].replace('"', '').split(",")
+        newData = []
+        
+        if len(lineData) > 2:
+            newData = [",".join(lineData[:len(lineData)-1]), lineData[len(lineData)-1]]
+        else:
+            newData = lineData
+        matrix.append(newData)
+    return matrix
+
+def getRandom():
+    
+    matrix = getTableMatrix()
+    percentages = []
+    
+    for i in range(2, len(matrix)):        
+        percentage = float(matrix[i][1])
+        percentages.append(percentage)
+
+    r = random.uniform(0, 99.8)
+    inc = 0
+    for i in range(len(percentages)):
+        inc += percentages[i]
+        if inc > r:
+            return matrix[i+2][0]
+    
 
 if __name__ == "__main__":
     app.debug = True
