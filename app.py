9dc0395b435d8a679a94981dcf96e3f598055018
Sebastian Cain <sebastian.cain.stuy@gmail.com>
2016-09-29 13:38:21 -0400

    fixed /uttil

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from utils.util import getTableMatrix, getRandom
 import random
 app = Flask(__name__)
 
@@ -11,43 +12,6 @@ def route1():
 
 def route2():
 	return render_template("table.html", title = "data/occupations", tableData = getTableMatrix(), occupation = getRandom())
-
-def getTableMatrix():
-    occu = open("occupations.csv", "r")
-    streamy = occu.read()
-
-    matrix = [[]]
-    
-    data = streamy.strip().split("\n")
-
-    for line in range(len(data)):
-        
-        lineData = data[line].replace('"', '').split(",")
-        newData = []
-        
-        if len(lineData) > 2:
-            newData = [",".join(lineData[:len(lineData)-1]), lineData[len(lineData)-1]]
-        else:
-            newData = lineData
-        matrix.append(newData)
-    return matrix
-
-def getRandom():
-    
-    matrix = getTableMatrix()
-    percentages = []
-    
-    for i in range(2, len(matrix)):        
-        percentage = float(matrix[i][1])
-        percentages.append(percentage)
-
-    r = random.uniform(0, 99.8)
-    inc = 0
-    for i in range(len(percentages)):
-        inc += percentages[i]
-        if inc > r:
-            return matrix[i+2][0]
-    
 
 if __name__ == "__main__":
     app.debug = True
